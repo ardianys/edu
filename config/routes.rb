4e50cb9fa46686c5c8f18f027c97fdaeed4dcc24
ardianys <ardianys@gmail.com>
2024-02-27 09:42:11 +0700

    Add custom sessions controller for Devise and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :comments
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   resources :users
   resources :posts
   get "up" => "rails/health#show", as: :rails_health_check
