b8d194989c9345d9b30e07f372ada1b50035b815
ardianys <ardianys@gmail.com>
2024-02-28 09:30:40 +0700

    Add admin and user roles to User model, and update campaign views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
+
+  def admin?
+    role == 'ADMIN'
+  end
+
+  def user?
+    role == 'USER'
+  end
 end
